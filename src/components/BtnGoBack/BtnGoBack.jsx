8561bc53695b15deb96c60af506dc10efd61c562
Yevhenii Yakovenko <yakovenkob@gmail.com>
2023-06-22 01:40:48 +0300

    styled go back button

--- src/components/BtnGoBack/BtnGoBack.jsx
+++ src/components/BtnGoBack/BtnGoBack.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import { Button } from './BtnGoBack.styled';
 
 export const BtnGoBack = ({ back }) => {
   const navigate = useNavigate();
 
   return (
-    <button type="button" onClick={() => navigate(back)}>
+    <Button type="button" onClick={() => navigate(back)}>
       Go Back
-    </button>
+    </Button>
   );
 };
---
+++ src/components/BtnGoBack/BtnGoBack.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  color: red;
+  background-color: #ffecd1;
+  border-radius: 4px;
+`;
